7f7858672a8b667c44de1dbef911d702988d78fe
Uladzimir Khartaniuk <47224750+KillerVyva@users.noreply.github.com>
2022-07-11 20:30:43 +0200

    Update Constants.py

---
+++ Constants.py
@@ -0,0 +1,14 @@
+API_KEY = 'your_telegram_api_key..'
+
+login_1 = 'login..'
+password_1 = 'pass..'
+
+login_2 = 'login..'
+password_2 = 'pass..'
+
+login_3 = 'login..'
+password_3 = 'pass..'
+
+ 
+
+
